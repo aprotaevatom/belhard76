29c5c6f7ba7c2206a7b19565820284734f204145
ArtsemDev <thommy.python.dev@gmail.com>
2024-04-23 21:28:05 +0300

    ///

--- main.py
+++ main.py
@@ -1,6 +1,135 @@
-def foo():
-    foo()
+# a = 4.34354
+# print(pow(-4, 0.5))
 
 
-class Bar(object):
-    __slots__ = ()
+# a = -5.56465
+# print(abs(a))
+
+# a = float("-.112_342_342e10")
+# print(a)
+
+# print(float("inf"))
+# print(float("-inf"))
+# print(float("nan"))
+# print(float("-nan"))
+# print(float("nan") == float("nan"))
+
+# a = (2342342+2j)
+# print(a)
+# [-5, 256]
+
+# a = None
+# print(a is None)
+
+# print(2 == 2.0)
+# print(2 != 4)
+# print(2 > 4)
+# print(2 < 4)
+# print(2 >= 4)
+# print(2 <= 4)
+# print(2 is 2)
+# print(2 is not 2.0)
+
+# text = "abcde\t\nfghijkl"
+# print(text[slice(0, 5, 2)])
+# print(len(text))
+
+
+# a = "hello world"
+# b = "hello world"
+# print(a is b)
+
+# a = "Hello world"
+# print("Hell" not in a)
+
+# a = "hello"
+# b = "world"
+# c = a + b
+# print(c * -5)
+
+# a = r"hello\n\n\n\nworld"
+# print(a)
+
+name = "Alex"
+email = "alex@gmail.com"
+age = 34
+is_human = True
+# text = "Hello %(first_name)s email=%(email_address)s" % {"first_name": name, "email_address": email}
+# text = "Hello {first_name} your email {email_address}".format(first_name=name, email_address=email)
+# text = f"Hello {name} your email {email}"
+# print(text)
+
+# text = f"{343465.2378546:.3f} \N{fire}🫡"
+# print(text)
+# text = "Hello world".encode()
+# print(text.decode())
+
+
+# text = "Hello"
+# print(len(text.encode("ascii")))
+
+# text = "    "
+# print(text.isprintable())
+# print(text.islower())
+# print(text.isupper())
+# print(text.istitle())
+# print(text.isalpha())
+# print(text.isdigit())
+# print(text.isnumeric())
+# print(text.isdecimal())
+# print(text.isalnum())
+# print(text.isspace())
+# print(text.isprintable())
+# print(text.isascii())
+# print(text.isidentifier())
+# print(text.isalpha())
+# print(text.capitalize())
+# print(text.lower())
+# print(text.upper())
+# print(text.title())
+# print(text.swapcase())
+# print(text.casefold())
+# text = "Hello\tworldrewteww\tpython"
+# print(text.count("ell", 2, 7))
+# print(text.center(11, "-"))
+# print(text.ljust(11, "-"))
+# print(text.rjust(11, "-"))
+# print(text.zfill(11))
+# print(text.expandtabs(8))
+# print("Hello".endswith("llo"))
+# print("Hello".startswith("Hell"))
+# print("Hello".removeprefix("Hell"))
+# print("Hello".removesuffix("Hell"))
+# print("Hello World Python".replace(" ", "---", 1))
+# print("Hello Hello".find("el"))
+# print("Hello Hello".rfind("el"))
+# print("Hello Hello".index("elll"))
+# print("Hello Hello".rindex("el"))
+
+# print("Hello--World--Python".split("--", 1))
+# print("Hello--World--Python".rsplit("--", 1))
+# print("Hello--World--Python".partition("--"))
+# print("Hello--World--Python".rpartition("--"))
+# print("--".join("HELLO"))
+
+# text = "-.,-,-..Hello-..,.world ,.,.,-python-,.,-,-,"
+# print(text.lstrip(".,-"))
+# print(text.rstrip(".,-"))
+# print(text.strip(".,-"))
+
+# text = "Hello\nworld"
+# print(text.splitlines())
+
+# print(ord("\n"))
+# print(chr(0))
+
+# print(bin(15))
+# print(bin(9))
+# print(bin(15 ^ 9))
+# print(~-4)
+# a = 32
+# print((a & (a-1) == 0 and a != 0))
+# print(bin(32))
+# print(bin(31))
+# print(32 & 31)
+# branch homework
